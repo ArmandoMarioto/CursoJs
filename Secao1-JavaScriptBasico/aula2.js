e29e2549efc64868122c8d0df9ade501e14574cc
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-11 11:23:37 -0300

    Aula 2 - Comentarios no codigo

---
+++ Secao1-JavaScriptBasico/aula2.js
@@ -0,0 +1,17 @@
+// Aula 2 - Comentarios no codigo
+
+// Comentario de uma linha
+
+/*
+    Comentario 
+    de
+    Varias 
+    Linhas
+*/
+
+console.log('Linha 1'); // comentario
+console.log('Linha 2'); // comentario 1
+console.log('Linha 2'); // comentario 2 
+console.log('Linha 2'); // comentario 3   
+
+//vai ser ignorado
