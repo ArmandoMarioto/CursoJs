d0089ebcd5f5c32854f54575d7b24dc293704b5d
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 11:43:16 -0300

    Aula 4 Variaveis Let

---
+++ Secao1-JavaScriptBasico/aula4/index.js
@@ -0,0 +1,13 @@
+// Aula 4 Variaveis Let
+
+let nome = 'João';   // String
+
+console.log(nome,' nasceu em 1984');
+console.log('em 2000 ',nome,' conheceu Maria.');
+console.log('Maria teve um 1 filho com',nome);
+console.log('o filho de ',nome,' se chama educardo.');
+
+nome = 'Armando';
+console.log(nome);
+nome = 'Rafael'
+console.log(nome);
