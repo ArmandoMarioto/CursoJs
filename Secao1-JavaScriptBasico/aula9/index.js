f4656a27c0dbfba3141ac8afce820500594c150c
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 12:59:14 -0300

    Aula 9 - Exercicios com variaveis

---
+++ Secao1-JavaScriptBasico/aula9/index.js
@@ -0,0 +1,11 @@
+// Aula 9 - Exercicios com variaveis
+
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+// isso se resolve com uma variavel temporaria ou assim ...
+
+[varA,varB,varC] = [varB,varC,varA];
+
+console.log(varA,varB,varC);// = B,C,A
+
