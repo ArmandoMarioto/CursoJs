8891fe47f294a2d24e0f16dc1f21f642759a6601
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 11:52:09 -0300

    Aula 5 - Constantes com const

---
+++ Secao1-JavaScriptBasico/aula5/index.js
@@ -0,0 +1,16 @@
+// Aula 5 - Constantes com const
+
+
+const nome = 'João';
+
+console.log(nome);
+//  nome = 'Armando' não pode mudar valor da constante
+console.log(nome);
+//  nome = 'Joãozinho'
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 7;
+const resultado = primeiroNumero * segundoNumero;
+
+console.log(resultado);
