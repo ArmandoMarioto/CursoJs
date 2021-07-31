e0a828e7049a6c8b37dbc32ee81e7a7d59eb98b9
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 12:47:50 -0300

    Aula 8 - Exercicios - solução

---
+++ Secao1-JavaScriptBasico/aula8/js/script.js
@@ -0,0 +1,16 @@
+// Aula 8 - Exercicios - solução
+
+let num1 = prompt('Digite um numero.'); // prompt sempre retorna uma string
+let num2 = prompt('Digite outro numero.');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2 ;
+
+
+alert('O resultado da sua conta é :' + resultado);
+alert(`O resultado da sua conta é : ${resultado}`);
+alert(`O resultado da sua conta é : ${num1 + num2}`);
+
+
