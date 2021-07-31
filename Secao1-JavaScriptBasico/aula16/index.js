712c8293feace59f350f2de6c919650cef52bbfc
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 19:49:51 -0300

    Aula 16 - Funções

---
+++ Secao1-JavaScriptBasico/aula16/index.js
@@ -0,0 +1,15 @@
+// Aula 16 - Funções
+
+function saudacao(text){
+    console.log('Bom dia',text);
+}
+saudacao('Armando');
+
+
+
+function soma(x,y)
+{
+    return x+y
+}
+
+console.log(`A soma de 5+4 é : ${soma(5,4)}`)
