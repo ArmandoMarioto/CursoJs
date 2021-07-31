66121b900ae0245c48ea255a109b2b807a37eb6c
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 12:41:52 -0300

    Aula 7 - alert, confirm e prompt

---
+++ Secao1-JavaScriptBasico/aula7/js/script.js
@@ -0,0 +1,14 @@
+// Aula 7 - alert, confirm e prompt
+
+alert('Com a nossa menssagem.');
+
+//retorna false ou true
+confirm('Deseja realemente apagar?');
+
+
+prompt('Digite o seu nome.');
+
+
+const confirma = confirm('Realmente deseja apagar?');
+
+console.log(confirma);
