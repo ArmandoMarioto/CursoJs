7cc8beb61e1f10ec20094e4c9a90b36b46e8e5df
ArmandoMarioto <armandomarioto@gmail.com>
2021-07-31 14:31:29 -0300

    Aula 12 - Mais sobre numbers

---
+++ Secao1-JavaScriptBasico/aula12/index.js
@@ -0,0 +1,20 @@
+// Aula 12 - Mais sobre numbers
+
+let num1 = 10.24121212123; // number
+let num2 = 2.5;  // number
+let num3 = 0.7; // number
+let num4 = 0.1;  // number
+
+
+// console.log(num1.toString()+num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+
+//console.log(num1.toFixed(2));
+
+
+num3 += num4;
+num3 += num4;
+num3 += num4;
+
+console.log(num3.toFixed())
