9211cc5c0be3cad7f2d5990e417f1f9c5e7adedf
ArmandoMarioto <armandomarioto@gmail.com>
2021-08-19 20:29:34 -0300

    Aula 1 - Operadores de comparação

---
+++ Secao2-JavaScriptLogica/aula1/index.js
@@ -0,0 +1,22 @@
+ /*
+ Aula 1 - Operadores de comparação
+ Operadores de comparação
+ > Maior que
+ >= maior que ou igual a
+ < menor que
+ <= menor que ou igual a
+ == igualdade
+ === igualdade estrita (valor e tipo)
+ != diferente
+ !== diferente estrita
+ */
+
+ const comp = 10 > 5;
+ console.log(comp);
+ console.log(10 >= 11);
+ console.log(10 < 11);
+ console.log(10 <= 11);
+ console.log(10 == '10');
+ console.log(10 === '10');
+ console.log(10 != 11);
+ console.log(10 !== 11);
