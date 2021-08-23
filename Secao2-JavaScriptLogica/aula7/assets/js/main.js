0a98e07fa937abfbc7d3bb58b95c77c0baa4bc30
ArmandoMarioto <armandomarioto@gmail.com>
2021-08-23 19:55:49 -0300

    Aula 7 - exercicio IMC

--- Secao2-JavaScriptLogica/aula7/assets/js/main.js
+++ Secao2-JavaScriptLogica/aula7/assets/js/main.js
@@ -22,13 +22,27 @@ form.addEventListener('submit',function (e){
     }
 
     const imc = getImc(peso, altura);
-    console.log(imc);
+    const nivelImc = getNivelImc(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+    setResultado(msg, true);
 
 });
 
 function getNivelImc(imc){
     const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
-    
+    if (imc >= 39.9){
+        return nivel[5];
+    }else if(imc >=34.9){
+        return nivel[4];
+    }else if(imc >=29.9){
+        return nivel[3];
+    }else if(imc >=24.9){
+        return nivel[2];
+    }else if(imc >=18.5){
+        return nivel[1];
+    }else if(imc < 18.5){
+        return nivel[0];
+    }
 }
 
 function getImc(peso , altura){
@@ -46,6 +60,13 @@ function setResultado(msg, isValid){
     //resultado.innerHTML = `<p>${msg}</p>`;
     resultado.innerHTML = '';
     const p = criaP();
+
+    if(isValid) 
+    {
+        p.classList.add('paragrafo-resultado');
+    }else {
+        p.classList.add('bad');
+    }
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
