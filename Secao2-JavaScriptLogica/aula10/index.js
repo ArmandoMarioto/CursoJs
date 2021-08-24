03345d58a762f68ca2de53129a6d69baf90c0c34
ArmandoMarioto <armandomarioto@gmail.com>
2021-08-24 20:56:45 -0300

    Aula 10 - Switch/Case

---
+++ Secao2-JavaScriptLogica/aula10/index.js
@@ -0,0 +1,38 @@
+/* 
+    Aula 10 - Switch/Case
+*/
+
+const data =  new Date('1987-04-21 00:00:00');
+const diaSemana = data.getDay();
+
+let diaSemanaTexto;
+
+switch (diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo.';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda.';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça.';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta.';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta.';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta.';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado.';
+        break;
+    default:
+        diaSemanaTexto = 'Dia indefinido.';
+        break;
+}
+
+console.log(diaSemanaTexto);
+    
